86e02cd21dc13acf51765cdc431bca10ecc328b4
Katie Lonsdale <katie952@gmail.com>
2023-05-22 11:28:31 -0600

    feat: add validations for passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   validates :email, email: true
 
+  validates_presence_of :password_digest
+
+  has_secure_password
+
   has_many :user_parties
   has_many :parties, through: :user_parties
 
