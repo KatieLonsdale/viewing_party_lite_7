4253457d723f896f83fb8d9339e051f7b63f0593
Katie Lonsdale <katie952@gmail.com>
2023-05-22 11:43:37 -0600

    feat: add password and password confirmation fields to form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   validates_presence_of :email
   validates_uniqueness_of :email
   validates :email, email: true
-
   validates_presence_of :password_digest
 
   has_secure_password
