eaf477ad1185b490bc6a507c2c7f25f6a7a0fd23
Katie Lonsdale <katie952@gmail.com>
2023-05-23 13:49:58 -0600

    add passwords to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@ UserParty.destroy_all
 User.destroy_all
 Party.destroy_all
 
-@user_1 = User.create!(name: "User 1", email: "email1@email.com")
-@user_2 = User.create!(name: "User 2", email: "email2@email.com")
-@user_3 = User.create!(name: "User 3", email: "email3@email.com")
-@user_4 = User.create!(name: "User 4", email: "email4@email.com")
-@user_5 = User.create!(name: "User 5", email: "email5@email.com")
+@user_1 = User.create!(name: "User 1", email: "email1@email.com", password: "test123", password_confirmation: "test123")
+@user_2 = User.create!(name: "User 2", email: "email2@email.com", password: "test123", password_confirmation: "test123")
+@user_3 = User.create!(name: "User 3", email: "email3@email.com", password: "test123", password_confirmation: "test123")
+@user_4 = User.create!(name: "User 4", email: "email4@email.com", password: "test123", password_confirmation: "test123")
+@user_5 = User.create!(name: "User 5", email: "email5@email.com", password: "test123", password_confirmation: "test123")
 
 @party_1 = Party.create!(duration: 200, date: "Tue, 9 May 2023", time: "12:00", movie_id: 238)
 @party_2 = Party.create!(duration: 180, date: "Mon, 16 Jan 2023", time: "18:00", movie_id: 550)
