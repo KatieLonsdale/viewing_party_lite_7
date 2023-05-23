fcbc048882a4f150f151f748fc9d2360db3feb08
Katie Lonsdale <katie952@gmail.com>
2023-05-23 11:12:57 -0600

    chore: moved tests to new spec in sessions directory

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -1,29 +1,29 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'login page' do
-  describe 'as a registered user, when I visit the log in page' do
-    it 'I can input valid credentials and be taken to my dashboard' do
-      test_data
-      visit new_session_path
+# RSpec.describe 'login page' do
+#   describe 'as a registered user, when I visit the log in page' do
+#     it 'I can input valid credentials and be taken to my dashboard' do
+#       test_data
+#       visit new_session_path
 
-      fill_in "Email", with: @user_1.email
-      fill_in "Password", with: @user_1.password
+#       fill_in "Email", with: @user_1.email
+#       fill_in "Password", with: @user_1.password
 
-      click_button "Log In"
+#       click_button "Log In"
 
-      expect(current_path).to eq("/users/#{@user_1.id}")
-    end
-  end
-  it 'flashes an error if my credentials are not correct' do
-    test_data
-    visit new_session_path
+#       expect(current_path).to eq("/users/#{@user_1.id}")
+#     end
+#   end
+#   it 'flashes an error if my credentials are not correct' do
+#     test_data
+#     visit new_session_path
 
-    fill_in "Email", with: @user_1.email
-    fill_in "Password", with: 'incorrect password'
+#     fill_in "Email", with: @user_1.email
+#     fill_in "Password", with: 'incorrect password'
 
-    click_button "Log In"
+#     click_button "Log In"
 
-    expect(current_path).to eq(new_session_path)
-    expect(page).to have_content("Incorrect credentials.")
-  end
-end
+#     expect(current_path).to eq(new_session_path)
+#     expect(page).to have_content("Incorrect credentials.")
+#   end
+# end
