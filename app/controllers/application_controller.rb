23ebd9480491fd96320594d2baaf6e74d057b5a1
Katie Lonsdale <katie952@gmail.com>
2023-05-23 11:14:59 -0600

    feat: add current_user action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 # All controllers inherit from this
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    @_current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
   def welcome
     @users = User.all
   end
