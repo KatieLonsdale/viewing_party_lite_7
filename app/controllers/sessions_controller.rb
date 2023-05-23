7e261f1ebbf5e7ad7311be3df6a3730f1b293c5e
Katie Lonsdale <katie952@gmail.com>
2023-05-23 11:13:48 -0600

    feat: create sessions controller with create and new actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to ("/users/#{user.id}")
+    else
+      redirect_to(new_session_path)
+      flash[:alert] = "Incorrect credentials."
+    end
+  end
+end
